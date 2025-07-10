e742f176410fcc1f78f378afc91c15a130021529
rurifu <saichaiphouvong@gmail.com>
2025-07-11 02:11:15 +1000

    Board mouse input
    The mouse detection is still not working properly. It is not detecting out of bound and the board grid is not aligned. Incomplete

--- Scene/board.gd
+++ Scene/board.gd
@@ -1,6 +1,9 @@
 extends Sprite2D
 
 const BOARD_SIZE = 8
+const CELL_WIDTH = 16
+
+const TEXTURE_HOLDER = preload("res://Scene/texture_holder.tscn")
 
 #White Piece
 const W_BISHOP = preload("res://Chess Asset/pixel chess_v1.2/16x32 pieces/W_Bishop.png")
@@ -26,10 +29,68 @@ const WHITE_TURN_DOT = preload("res://Chess Asset/pixel chess_v1.2/32x32 pieces/
 @onready var turn: Sprite2D = $Turn
 
 #Variable
+# -6 = black king
+# -5 = black queen
+# -4 = black rook
+# -3 = black bishop
+# -2 = black knight
+# -1 = black pawn
+# 0 = empty
+# 6 = white king
+# 5 = white queen
+# 4 = white rook
+# 3 = white bishop
+# 2 = white knight
+# 1 = white pawn
+
 var board : Array
-	
+var white : bool
+var state : bool
+var moves = []
+var selected_piece : Vector2
+
 func _ready():
-	pass
+	board.append([4, 2, 3, 5, 6, 3, 2, 4])
+	board.append([1, 1, 1, 1, 1, 1, 1, 1])
+	board.append([0, 0, 0, 0, 0, 0, 0, 0])
+	board.append([0, 0, 0, 0, 0, 0, 0, 0])
+	board.append([0, 0, 0, 0, 0, 0, 0, 0])
+	board.append([0, 0, 0, 0, 0, 0, 0, 0])
+	board.append([-1, -1, -1, -1, -1, -1, -1, -1])
+	board.append([-4, -2, -3, -6, -5, -3, -2, -4])
 	
-func _process(delta):
-	pass
+	display_board()
+	
+func _input(event):
+	if event is InputEventMouseButton && event.pressed:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if if_mouse_out(): return
+			var var1 = snapped(get_global_mouse_position().x+8, 0) / 16
+			var var2 = abs(snapped(get_global_mouse_position().y-8, 0)) / 16
+			print(var1, "  ", var2)
+
+func if_mouse_out():
+	if get_global_mouse_position().x < 8 || get_global_mouse_position().x > 134 || get_global_mouse_position().y > 8 || get_global_mouse_position().y < -134:
+		return false
+
+func display_board():
+	for i in BOARD_SIZE:
+		for j in BOARD_SIZE:
+			var holder = TEXTURE_HOLDER.instantiate()
+			pieces.add_child(holder)
+			holder.global_position = Vector2(j * CELL_WIDTH + (CELL_WIDTH / 2)+8, -i * CELL_WIDTH - (CELL_WIDTH / 2)-16)
+			
+			match board[i][j]:
+				-6: holder.texture = B_KING
+				-5: holder.texture = B_QUEEN
+				-4: holder.texture = B_ROOK
+				-3: holder.texture = B_BISHOP
+				-2: holder.texture = B_KNIGHT
+				-1: holder.texture = B_PAWN
+				0: holder.texture = null
+				6: holder.texture = W_KING
+				5: holder.texture = W_QUEEN
+				4: holder.texture = W_ROOK
+				3: holder.texture = W_BISHOP
+				2: holder.texture = W_KNIGHT
+				1: holder.texture = W_PAWN
