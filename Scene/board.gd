46b81459e7f935cb8d3180652ef336fb885f243d
Chilliwen32 <Chilliwen32@gmail.com>
2025-07-10 23:16:52 +1000

    Added all the pieces
    All the pieces have been loaded to the script.

--- Scene/board.gd
+++ Scene/board.gd
@@ -2,9 +2,21 @@ extends Sprite2D
 
 const BOARD_SIZE = 8
 
-const BLACK_PIECES = preload("res://Chess Asset/pixel chess_v1.2/16x16 pieces/BlackPieces.png")
+#White Piece
+const W_BISHOP = preload("res://Chess Asset/pixel chess_v1.2/16x32 pieces/W_Bishop.png")
+const W_KING = preload("res://Chess Asset/pixel chess_v1.2/16x32 pieces/W_King.png")
+const W_KNIGHT = preload("res://Chess Asset/pixel chess_v1.2/16x32 pieces/W_Knight.png")
+const W_PAWN = preload("res://Chess Asset/pixel chess_v1.2/16x32 pieces/W_Pawn.png")
+const W_QUEEN = preload("res://Chess Asset/pixel chess_v1.2/16x32 pieces/W_Queen.png")
+const W_ROOK = preload("res://Chess Asset/pixel chess_v1.2/16x32 pieces/W_Rook.png")
 
-const WHITE_PIECES = preload("res://Chess Asset/pixel chess_v1.2/16x16 pieces/WhitePieces.png")
+#Black Piece
+const B_BISHOP = preload("res://Chess Asset/pixel chess_v1.2/16x32 pieces/B_Bishop.png")
+const B_KING = preload("res://Chess Asset/pixel chess_v1.2/16x32 pieces/B_King.png")
+const B_KNIGHT = preload("res://Chess Asset/pixel chess_v1.2/16x32 pieces/B_Knight.png")
+const B_PAWN = preload("res://Chess Asset/pixel chess_v1.2/16x32 pieces/B_Pawn.png")
+const B_QUEEN = preload("res://Chess Asset/pixel chess_v1.2/16x32 pieces/B_Queen.png")
+const B_ROOK = preload("res://Chess Asset/pixel chess_v1.2/16x32 pieces/B_Rook.png")
 
 @onready var pieces: Node2D = $Pieces
 @onready var dot: Node2D = $Dot
