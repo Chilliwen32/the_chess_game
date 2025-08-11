a8cb93ee678fc4855ebb6abe9b8e1764abb8a93c
rurifu <saichaiphouvong@gmail.com>
2025-08-12 03:16:46 +1000

    ADDED ROOK MOVE (INCOMPLETE)

--- Scene/board.gd
+++ Scene/board.gd
@@ -46,14 +46,14 @@ const PIECE_MOVE = preload("res://Chess Asset/pixel chess_v1.2/32x32 pieces/Piec
 # 1 = white pawn
 
 var board : Array
-var white : bool
-var state : bool
+var white : bool = true
+var state : bool = false
 var moves = []
 var selected_piece : Vector2
 
 func _ready():
 	board.append([4, 2, 3, 5, 6, 3, 2, 4])
-	board.append([1, 1, 1, 1, 1, 1, 1, 1])
+	board.append([0, 0, 0, 0, 0, 0, 0, 0])
 	board.append([0, 0, 0, 0, 0, 0, 0, 0])
 	board.append([0, 0, 0, 0, 0, 0, 0, 0])
 	board.append([0, 0, 0, 0, 0, 0, 0, 0])
@@ -66,20 +66,21 @@ func _ready():
 func _input(event):
 	if event is InputEventMouseButton && event.pressed:
 		if event.button_index == MOUSE_BUTTON_LEFT:
-			if if_mouse_out(): return
-			var var1 = snapped(get_global_mouse_position().x+8, 0) / 16
-			var var2 = abs(snapped(get_global_mouse_position().y-8, 0)) / 16
-			
+			if is_mouse_out(): return
+			var var1 = (snapped(get_global_mouse_position().x+8, 0) / 16) - 1
+			var var2 = (abs(snapped(get_global_mouse_position().y-8, 0)) / 16) - 1
 			if !state && (white && board[var2][var1] > 0 || white && board[var2][var1] < 0):
-				show_options()
 				selected_piece = Vector2(var2,var1)
+				show_options()
 				state = true
-			elif state: set_move(var2, var1)
+			elif state: 
+				set_move(var2, var1)
 			
 
-func if_mouse_out():
-	if get_global_mouse_position().x < 8 || get_global_mouse_position().x > 134 || get_global_mouse_position().y > 8 || get_global_mouse_position().y < -134:
-		return false
+func is_mouse_out():
+	if get_global_mouse_position().x < 8 || get_global_mouse_position().x > 135 || get_global_mouse_position().y > 8 || get_global_mouse_position().y < -134:
+		return true
+	return false
 
 func display_board():
 	for child in pieces.get_children():
@@ -122,8 +123,8 @@ func set_move(var2, var1):
 			white = !white
 			display_board()
 			break
-		delete_dots()
-		state = false
+	delete_dots()
+	state = false
 
 func delete_dots():
 	for child in dots.get_children():
@@ -131,10 +132,63 @@ func delete_dots():
 
 func show_dots():
 	for i in moves:
-		var holder = Sprite2D.new()
+		var holder = TEXTURE_HOLDER.instantiate()
 		dots.add_child(holder)
 		holder.texture = PIECE_MOVE
-		holder.global_position = Vector2(i.y * CELL_WIDTH + (CELL_WIDTH / 2) + 8, -i.x * CELL_WIDTH + (CELL_WIDTH / 2) - 16)
+		holder.global_position = Vector2(i.y * CELL_WIDTH + (CELL_WIDTH / 2) + 8, -i.x * CELL_WIDTH - (CELL_WIDTH / 2) - 16)
 
 func get_moves():
+	var _moves = []
+	match abs(board[selected_piece.x][selected_piece.y]):
+		1: print("pawn")
+		2: print("knight")
+		3: print("bishop")
+		4: _moves = get_rook_moves()
+		5: print("queen")
+		6: print("king")	
+	return _moves
+
+func is_valid_position(pos : Vector2):
+	if pos.x >= 0 && pos.x < BOARD_SIZE && pos.y >= 0 && pos.y < BOARD_SIZE:
+		return true
+	return false
+
+func is_empty(pos : Vector2):
+	if board[pos.x][pos.y] == 0:
+		return state
+	return false
+ 
+func is_enemy(pos : Vector2):
+	if white && board[pos.x][pos.y] < 0 || !white && board[pos.x][pos.y] > 0:
+		return true
+	return false
+
+func get_rook_moves():
+	var _moves = []
+	var directions = [Vector2(0, 1), Vector2(0, -1), Vector2(1, 0), Vector2(-1, 0)]
+	
+	for i in directions:
+		var pos = selected_piece
+		pos += i
+		while is_valid_position(pos):
+			if is_empty(pos):
+				_moves.append(pos)
+			elif is_enemy(pos):
+				_moves.append(pos)
+				break
+			else:
+				break
+			pos += i
+			
+	return _moves
+
+func get_bishop_moves():
+	pass
+func get_knight_moves():
+	pass
+func get_king_moves():
+	pass
+func get_queen_moves():
+	pass
+func get_pawn_moves():
 	pass
